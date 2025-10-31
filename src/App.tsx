fd4915c653608151b33f1e52c1fe9e4fa83b4888
Dana Yatsuta <danayatsuta@gmail.com>
2025-10-31 23:41:28 +0300

    Remove unnecessary image imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
-import logo from "./assets/logo.svg";
-import iconUnits from "./assets/icon-units.svg";
-import iconDropdown from "./assets/icon-dropdown.svg";
-import iconSearch from "./assets/icon-search.svg";
-
 function App() {
   return (
     <>
       <header className="flex justify-between pt-4">
-        <img src={logo} alt="Weather Now logo" className="w-[138px]" />
+        <img
+          src="/src/assets/logo.svg"
+          alt="Weather Now logo"
+          className="w-[138px]"
+        />
 
         <button
           type="button"
           className="flex h-8 items-center gap-1.5 rounded-md bg-neutral-800 px-2.5 text-sm"
         >
-          <img src={iconUnits} aria-hidden="true" className="h-3.5" />
+          <img
+            src="/src/assets/icon-units.svg"
+            aria-hidden="true"
+            className="h-3.5"
+          />
           Units
-          <img src={iconDropdown} aria-hidden="true" className="h-1.5" />
+          <img
+            src="/src/assets/icon-dropdown.svg"
+            aria-hidden="true"
+            className="h-1.5"
+          />
         </button>
       </header>
 
@@ -31,7 +38,7 @@ function App() {
           className="flex flex-col gap-3 pt-12 text-xl"
         >
           <label className="flex h-14 items-center gap-4 rounded-lg bg-neutral-800 px-6">
-            <img src={iconSearch} aria-hidden="true" />
+            <img src="/src/assets/icon-search.svg" aria-hidden="true" />
 
             <input
               type="text"
