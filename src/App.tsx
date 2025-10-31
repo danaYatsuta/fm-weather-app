32dcc2d5dc9709a2293639a63081256655284b8c
Dana Yatsuta <danayatsuta@gmail.com>
2025-10-31 22:12:00 +0300

    Add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import logo from "./assets/logo.svg";
+import iconUnits from "./assets/icon-units.svg";
+import iconDropdown from "./assets/icon-dropdown.svg";
+
 function App() {
-  return <p>Hello World</p>;
+  return (
+    <>
+      <header className="flex justify-between px-4 pt-4">
+        <img src={logo} alt="Weather Now logo" className="w-[138px]" />
+
+        <button
+          type="button"
+          className="flex h-8 items-center gap-1.5 rounded-md bg-neutral-800 px-2.5 text-sm font-medium"
+        >
+          <img src={iconUnits} aria-hidden="true" className="h-3.5" />
+          Units
+          <img src={iconDropdown} aria-hidden="true" className="h-1.5" />
+        </button>
+      </header>
+    </>
+  );
 }
 
 export default App;
