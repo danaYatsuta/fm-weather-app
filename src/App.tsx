ea68dcac43971edbbe0781db3091f0a7cb3b09aa
Dana Yatsuta <danayatsuta@gmail.com>
2025-11-01 06:45:55 +0300

    Responsive header

--- src/App.tsx
+++ src/App.tsx
@@ -24,27 +24,27 @@ function App() {
 
   return (
     <>
-      <header className="flex justify-between pt-4">
+      <header className="flex justify-between pt-4 xl:pt-12">
         <img
           src="/src/assets/logo.svg"
           alt="Weather Now logo"
-          className="w-[138px]"
+          className="w-[138px] xl:w-auto"
         />
 
         <button
           type="button"
-          className="flex h-8 items-center gap-1.5 rounded-md bg-neutral-800 px-2.5 text-sm"
+          className="flex h-8 items-center gap-1.5 rounded-md bg-neutral-800 px-2.5 text-sm xl:h-11 xl:gap-2.5 xl:px-4 xl:text-base"
         >
           <img
             src="/src/assets/icon-units.svg"
             aria-hidden="true"
-            className="h-3.5"
+            className="h-3.5 xl:h-auto"
           />
           Units
           <img
             src="/src/assets/icon-dropdown.svg"
             aria-hidden="true"
-            className="h-1.5"
+            className="h-1.5 xl:h-auto"
           />
         </button>
       </header>
