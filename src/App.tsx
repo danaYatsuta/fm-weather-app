2802d274a31932834218fe0b47e93fc2f120c437
Dana Yatsuta <danayatsuta@gmail.com>
2025-11-01 06:27:07 +0300

    Add hourly forecast cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import WeatherDetailCard from "./components/WeatherDetailCard";
 import DailyForecastCard from "./components/DailyForecastCard";
+import HourlyForecastCard from "./components/HourlyForecastCard";
 
 function App() {
+  const hourlyForecastEntries = [...Array(24).keys()].map((x) => {
+    const time = (x % 12) + 1;
+    let period;
+
+    if (x < 11) period = "AM";
+    else if (x === 11) period = "noon";
+    else if (x < 23) period = "PM";
+    else period = "midnight";
+
+    return (
+      <HourlyForecastCard
+        time={`${time} ${period}`}
+        icon="sunny"
+        temp="20°"
+        key={x}
+      />
+    );
+  });
+
   return (
     <>
       <header className="flex justify-between pt-4">
@@ -127,6 +147,24 @@ function App() {
               tempDay="24°"
               tempNight="15°"
             />
+          </div>
+        </section>
+
+        <section className="mt-8 mb-12 flex h-[685px] flex-col gap-4 rounded-2xl bg-neutral-800 px-4 py-5">
+          <div className="flex items-center justify-between">
+            <h2 className="text-xl font-semibold">Hourly forecast</h2>
+
+            <button
+              type="button"
+              className="flex h-9 items-center gap-2.5 rounded-md bg-neutral-600 px-4 text-base"
+            >
+              Tuesday
+              <img src="/src/assets/icon-dropdown.svg" aria-hidden="true" />
+            </button>
+          </div>
+
+          <div className="flex flex-col gap-4 overflow-scroll">
+            {hourlyForecastEntries}
           </div>
         </section>
       </main>
---
+++ src/components/HourlyForecastCard.tsx
@@ -0,0 +1,42 @@
+interface HourlyForecastCardProps {
+  time: string;
+  icon:
+    | "drizzle"
+    | "fog"
+    | "overcast"
+    | "partly-cloudy"
+    | "rain"
+    | "snow"
+    | "storm"
+    | "sunny";
+  temp: string;
+}
+
+const iconAlts = {
+  drizzle: "Drizzle icon",
+  fog: "Fog icon",
+  overcast: "Overcast icon",
+  "partly-cloudy": "Partly cloudy icon",
+  rain: "Rain icon",
+  snow: "Snow icon",
+  storm: "Storm icon",
+  sunny: "Sunny icon",
+};
+
+function HourlyForecastCard({ time, icon, temp }: HourlyForecastCardProps) {
+  const iconSrc = `/src/assets/icon-${icon}.webp`;
+  const iconAlt = iconAlts[icon];
+
+  return (
+    <div className="flex h-[60px] shrink-0 items-center justify-between rounded-md border border-neutral-600 bg-neutral-700 pr-4 pl-2.5">
+      <div className="flex items-center gap-2 text-xl">
+        <img src={iconSrc} alt={iconAlt} className="size-10" />
+        <p>{time}</p>
+      </div>
+
+      <p className="text-base">{temp}</p>
+    </div>
+  );
+}
+
+export default HourlyForecastCard;
