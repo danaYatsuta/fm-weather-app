dd306d5d0cfd27409f73886c957357f31a44c222
Dana Yatsuta <danayatsuta@gmail.com>
2025-11-02 21:09:35 +0300

    Add example API responses and response types

---
+++ src/types.d.ts
@@ -0,0 +1,91 @@
+export interface GeocodingResponse {
+  results: Result[];
+  generationtime_ms: number;
+}
+
+interface Result {
+  id: number;
+  name: string;
+  latitude: number;
+  longitude: number;
+  elevation: number;
+  feature_code: string;
+  country_code: string;
+  admin1_id: number;
+  admin2_id?: number;
+  admin3_id?: number;
+  admin4_id?: number;
+  timezone: string;
+  population: number;
+  postcodes: string[];
+  country_id: number;
+  country: string;
+  admin1: string;
+  admin2?: string;
+  admin3?: string;
+  admin4?: string;
+}
+
+export interface WeatherResponse {
+  latitude: number;
+  longitude: number;
+  generationtime_ms: number;
+  utc_offset_seconds: number;
+  timezone: string;
+  timezone_abbreviation: string;
+  elevation: number;
+  current_units: CurrentUnits;
+  current: Current;
+  hourly_units: HourlyUnits;
+  hourly: Hourly;
+  daily_units: DailyUnits;
+  daily: Daily;
+}
+
+interface Current {
+  time: string;
+  interval: number;
+  weather_code: number;
+  temperature_2m: number;
+  apparent_temperature: number;
+  relative_humidity_2m: number;
+  wind_speed_10m: number;
+  precipitation: number;
+}
+
+interface CurrentUnits {
+  time: string;
+  interval: string;
+  weather_code: string;
+  temperature_2m: string;
+  apparent_temperature: string;
+  relative_humidity_2m: string;
+  wind_speed_10m: string;
+  precipitation: string;
+}
+
+interface Daily {
+  time: string[];
+  weather_code: number[];
+  temperature_2m_max: number[];
+  temperature_2m_min: number[];
+}
+
+interface DailyUnits {
+  time: string;
+  weather_code: string;
+  temperature_2m_max: string;
+  temperature_2m_min: string;
+}
+
+interface Hourly {
+  time: string[];
+  weather_code: number[];
+  temperature_2m: number[];
+}
+
+interface HourlyUnits {
+  time: string;
+  weather_code: string;
+  temperature_2m: string;
+}
