007893f3bb27a64216724fb56dc289cf146ef2b2
Dana Yatsuta <danayatsuta@gmail.com>
2025-11-04 00:43:29 +0300

    Install react compiler and enable it for build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,15 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [
+      react({
+        babel: {
+          plugins: command === "build" ? ["babel-plugin-react-compiler"] : [],
+        },
+      }),
+      tailwindcss(),
+    ],
+  };
 });
