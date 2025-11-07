872bcf0987859710178545a925b8ef5fd2b055d3
Dana Yatsuta <danayatsuta@gmail.com>
2025-11-07 20:48:36 +0300

    Install and use react query in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useState } from "react";
 
 import type {
   LocationInfo,
@@ -14,15 +14,9 @@ import AppError from "./components/AppError";
 import AppHeader from "./components/AppHeader";
 import AppHourlyForecast from "./components/AppHourlyForecast";
 import AppSearchForm from "./components/AppSearchForm";
+import { useQuery } from "@tanstack/react-query";
 
 function App() {
-  const [unitSystem, setUnitSystem] = useState<UnitSystem>("metric");
-  const [unitInfo, setUnitInfo] = useState<UnitInfo>({
-    temperatureUnit: "celsius",
-    windSpeedUnit: "kmh",
-    precipitationUnit: "mm",
-  });
-
   const [locationInfo, setLocationInfo] = useState<LocationInfo>({
     name: "Berlin",
     country: "Germany",
@@ -31,61 +25,43 @@ function App() {
     longitude: 13.41,
   });
 
-  const [weatherResponse, setWeatherResponse] =
-    useState<WeatherResponse | null>(null);
+  const [unitSystem, setUnitSystem] = useState<UnitSystem>("metric");
+  const [unitInfo, setUnitInfo] = useState<UnitInfo>({
+    temperatureUnit: "celsius",
+    windSpeedUnit: "kmh",
+    precipitationUnit: "mm",
+  });
 
-  const [isError, setIsError] = useState(false);
+  const url = `https://api.open-meteo.com/v1/forecast?`;
 
-  const fetchData = useCallback(async () => {
-    setWeatherResponse(null);
-    setIsError(false);
+  const params = new URLSearchParams([
+    [
+      "current",
+      "weather_code,temperature_2m,apparent_temperature,relative_humidity_2m,wind_speed_10m,precipitation",
+    ],
+    ["daily", "weather_code,temperature_2m_max,temperature_2m_min"],
+    ["hourly", "weather_code,temperature_2m"],
 
-    const url = `https://api.open-meteo.com/v1/forecast?`;
+    ["latitude", locationInfo.latitude.toString()],
+    ["longitude", locationInfo.longitude.toString()],
+    ["timezone", locationInfo.timezone],
+    ["temperature_unit", unitInfo.temperatureUnit],
+    ["wind_speed_unit", unitInfo.windSpeedUnit],
+    ["precipitation_unit", unitInfo.precipitationUnit],
+  ]);
 
-    const params = new URLSearchParams([
-      [
-        "current",
-        "weather_code,temperature_2m,apparent_temperature,relative_humidity_2m,wind_speed_10m,precipitation",
-      ],
-      ["daily", "weather_code,temperature_2m_max,temperature_2m_min"],
-      ["hourly", "weather_code,temperature_2m"],
-
-      ["latitude", locationInfo.latitude.toString()],
-      ["longitude", locationInfo.longitude.toString()],
-      ["timezone", locationInfo.timezone],
-      ["temperature_unit", unitInfo.temperatureUnit],
-      ["wind_speed_unit", unitInfo.windSpeedUnit],
-      ["precipitation_unit", unitInfo.precipitationUnit],
-    ]);
-
-    try {
+  const { error, data: weatherData } = useQuery({
+    queryKey: ["weatherData", locationInfo, unitInfo],
+    queryFn: async (): Promise<WeatherResponse> => {
+      console.log("fetching...");
       const response = await fetch(url + params);
 
-      if (!response.ok) {
-        if (import.meta.env.DEV)
-          console.error(
-            `Response failed, status: ${response.status} ${response.statusText}`,
-          );
+      if (!response.ok)
+        throw new Error(`${response.status} ${response.statusText}`);
 
-        setIsError(true);
-
-        return;
-      }
-
-      const result = await response.json();
-      setTimeout(() => {
-        setWeatherResponse(result);
-      }, 500);
-    } catch (error) {
-      if (error instanceof Error && import.meta.env.DEV)
-        console.error(error.message);
-      setIsError(true);
-    }
-  }, [locationInfo, unitInfo]);
-
-  useEffect(() => {
-    fetchData();
-  }, [fetchData]);
+      return await response.json();
+    },
+  });
 
   function handleUnitSystemChange(newUnitSystem: UnitSystem) {
     setUnitSystem(newUnitSystem);
@@ -114,9 +90,9 @@ function App() {
         onUnitInfoChange={setUnitInfo}
       />
 
-      {isError ? (
+      {error ? (
         <main className="my-28">
-          <AppError onRetryButtonClick={fetchData} />
+          <AppError onRetryButtonClick={() => {}} />
         </main>
       ) : (
         <main className="my-12 flex grid-cols-[800px_1fr] grid-rows-[0fr_0fr_286px_0fr_0fr] flex-col xl:my-[60px] xl:grid">
@@ -129,28 +105,28 @@ function App() {
           <AppCurrentWeatherCard
             locationName={locationInfo.name}
             locationCountry={locationInfo.country}
-            time={weatherResponse?.current.time}
-            weatherCode={weatherResponse?.current.weather_code}
-            temperature={weatherResponse?.current.temperature_2m}
+            time={weatherData?.current.time}
+            weatherCode={weatherData?.current.weather_code}
+            temperature={weatherData?.current.temperature_2m}
           />
           <AppCurrentWeatherDetails
-            feelsLikeTemperature={weatherResponse?.current.apparent_temperature}
-            humidity={weatherResponse?.current.relative_humidity_2m}
-            windSpeed={weatherResponse?.current.wind_speed_10m}
+            feelsLikeTemperature={weatherData?.current.apparent_temperature}
+            humidity={weatherData?.current.relative_humidity_2m}
+            windSpeed={weatherData?.current.wind_speed_10m}
             windSpeedUnit={unitInfo.windSpeedUnit}
-            precipitation={weatherResponse?.current.precipitation}
+            precipitation={weatherData?.current.precipitation}
             precipitationUnit={unitInfo.precipitationUnit}
           />
           <AppDailyForecast
-            times={weatherResponse?.daily.time}
-            weatherCodes={weatherResponse?.daily.weather_code}
-            maxTemps={weatherResponse?.daily.temperature_2m_max}
-            minTemps={weatherResponse?.daily.temperature_2m_min}
+            times={weatherData?.daily.time}
+            weatherCodes={weatherData?.daily.weather_code}
+            maxTemps={weatherData?.daily.temperature_2m_max}
+            minTemps={weatherData?.daily.temperature_2m_min}
           />
           <AppHourlyForecast
-            times={weatherResponse?.hourly.time}
-            weatherCodes={weatherResponse?.hourly.weather_code}
-            temperatures={weatherResponse?.hourly.temperature_2m}
+            times={weatherData?.hourly.time}
+            weatherCodes={weatherData?.hourly.weather_code}
+            temperatures={weatherData?.hourly.temperature_2m}
           />
         </main>
       )}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.tsx";
+
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { staleTime: 2 * 60 * 1000 } },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 );
