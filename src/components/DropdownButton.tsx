7509b4f3ce882480b28d467caa530dca74e3d0d8
Dana Yatsuta <danayatsuta@gmail.com>
2025-11-23 21:33:57 +0300

    Use useImperativeHandle in DropdownButton

--- src/components/DropdownButton.tsx
+++ src/components/DropdownButton.tsx
@@ -1,9 +1,15 @@
+import { useImperativeHandle, useRef } from "react";
+
 interface DropdownButtonProps {
   border?: boolean;
   children?: React.ReactNode;
   fullWidth?: boolean;
   onButtonClick?: React.MouseEventHandler;
-  ref?: React.Ref<HTMLButtonElement>;
+  ref?: React.Ref<DropdownButtonRef>;
+}
+
+interface DropdownButtonRef {
+  contains: (otherNode: Node | null) => boolean;
 }
 
 function DropdownButton({
@@ -13,13 +19,27 @@ function DropdownButton({
   onButtonClick,
   ref,
 }: DropdownButtonProps) {
+  /* ---------------------------------- Hooks --------------------------------- */
+
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  useImperativeHandle(ref, () => {
+    return {
+      contains(otherNode: Node | null) {
+        if (!buttonRef.current) return false;
+
+        return buttonRef.current.contains(otherNode);
+      },
+    };
+  });
+
   /* --------------------------------- Markup --------------------------------- */
 
   return (
     <button
       className={`${border ? "hover:border-neutral-600" : ""} ${fullWidth ? "w-full" : ""} h-10 rounded-md border border-neutral-800 px-2 text-left text-base outline-offset-1 hover:bg-neutral-700 focus:outline-1`}
       onClick={onButtonClick}
-      ref={ref}
+      ref={buttonRef}
       type="button"
     >
       {children}
