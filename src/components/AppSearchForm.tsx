2753f900395573269a5184074b32d7fbdbad982d
Dana Yatsuta <danayatsuta@gmail.com>
2025-11-25 22:04:07 +0300

    Text cursor on search input label

--- src/components/AppSearchForm.tsx
+++ src/components/AppSearchForm.tsx
@@ -182,7 +182,7 @@ export default function AppSearchForm({
         className="flex flex-col gap-3 text-xl xl:flex-row xl:justify-center xl:gap-4"
         onSubmit={handleSubmit}
       >
-        <label className="flex h-14 items-center gap-4 rounded-xl bg-neutral-800 px-6 outline-offset-3 hover:bg-neutral-700 has-focus-visible:outline-2 xl:w-131">
+        <label className="flex h-14 cursor-text items-center gap-4 rounded-xl bg-neutral-800 px-6 outline-offset-3 hover:bg-neutral-700 has-focus-visible:outline-2 xl:w-131">
           <img alt="" src={iconSearch} />
 
           <input
